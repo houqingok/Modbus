c0c7eea14ced8fc754f9ee10486eb4fe4b93ff6e
cdmask <cdunzhi@gmail.com>
2016-12-30 02:08:48 +0800

    Add lib

---
+++ modbus_master/dof6/include/calc_rod_length.h
@@ -0,0 +1,19 @@
+/*
+ * calc_rod_length.h
+ *
+ * Code generation for function 'calc_rod_length'
+ *
+ * C source code generated on: Tue Dec 27 14:53:21 2016
+ *
+ */
+
+#ifndef __CALC_ROD_LENGTH_H__
+#define __CALC_ROD_LENGTH_H__
+/* Include files */
+#include <math.h>
+#include"DataTypes.h"
+
+/* Function Declarations */
+extern void calc_rod_length(float32 trans[3], float32 orient[3],  float32 rod_attach_P[18],  float32 servo_attach_B[18]);
+#endif
+/* End of code generation (calc_rod_length.h) */
